8c145e01e2d81fd5163d7bcc87e71697e5a7ac8e
mmostafa886 <36827060+mmostafa886@users.noreply.github.com>
2021-10-13 16:24:45 +0200

    Updating the driver URL

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -63,7 +63,7 @@ public class TestBase{
 
        caps.setCapability("app" , System.getProperty("user.dir")+"/apps/app-sbk-releaseStaging.apk");
 
-        driver = new AndroidDriver(new URL("http://127.0.1.1:4723/wd/hub"),caps);
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"),caps);
     }
 
     public static void Android_Setup() throws MalformedURLException {
